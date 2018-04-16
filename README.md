a3f9927d0923a7f13dbd78dd3159f902593e25f2
Maki <mxmcube@gmail.com>
2018-04-16 13:17:11 +0100

    All in the README.md now

--- README.md
+++ README.md
@@ -6,7 +6,19 @@
 
 ## Install
 
-All instructions can be found at <a href='https://git.maki.cat/dracula-for-ddg'>https://makixx.github.io/duckduckgo</a>.
+Try it by visiting this link:
+
+https://duckduckgo.com/?k7=282a36&k8=f8f8f2&k9=50fa7b&kae=t&kt=p&ks=m&kw=n&km=l&ko=s&kj=282a36&ka=p&kaa=bd93f9&ku=-1&kx=f1fa8c&ky=44475a&kaf=1&kai=1&kf=1
+
+Save it by pasting this in the URL bar whilst browsing https://duckduckgo.com: **(You may have to write "javascript:" at the front manually)**
+
+```js
+javascript:var dracula=["7=282a36","8=f8f8f2","9=50fa7b","ae=t","t=p","s=m","w=n","m=l","o=s","j=282a36","a=p","aa=bd93f9","u=-1","x=f1fa8c","y=44475a","af=1","ai=1","f=1"];for(var i=0;i<dracula.length;i++)document.cookie=dracula[i];alert('Appearance settings have successfully been updated!');location.reload();
+```
+
+If you want to make your own changes, visit:
+
+https://duckduckgo.com/settings
 
 ## Team
 
---
+++ make.js
@@ -0,0 +1,48 @@
+var fs = require("fs");
+
+var settings = {
+	config: "config.js",
+	readme: "README.md",
+	readmeLine: {
+		bookmarklet: 11,
+		script: 16,
+	}
+}
+
+// ----------------------------------------------------------------
+
+var readme = fs.readFileSync(__dirname+"/"+settings.readme, "utf8").split("\n");
+config = require(__dirname+"/"+settings.config);
+
+// Make bookmarklet
+var bookmarklet = "https://duckduckgo.com/?"
+for (var i=0; i<Object.keys(config).length; i++) {
+	let key = Object.keys(config)[i];
+	let value = config[key];
+	bookmarklet += "k"+key+"="+value+"&";
+}
+bookmarklet = bookmarklet.substring(0, bookmarklet.length-1);
+
+readme[settings.readmeLine.bookmarklet-1] = bookmarklet
+console.log(settings.readme+" (line "+
+	settings.readmeLine.bookmarklet+"): "+bookmarklet)
+
+// Make javascipt: script
+cookies = [];
+for (var i = 0; i < Object.keys(config).length; i++) {
+	let key = Object.keys(config)[i];
+	let value = config[key];
+	cookies[i] = key+"="+value;
+}
+script = "javascript:var dracula="+
+	JSON.stringify(cookies)+";"+
+	"for(var i=0;i<dracula.length;i++)document.cookie=dracula[i];"+
+	"alert('Appearance settings have successfully been updated!');"+
+	"location.reload();"
+
+readme[settings.readmeLine.script-1] = script
+console.log(settings.readme+" (line "+
+	settings.readmeLine.script+"): "+script)
+
+// Write file
+fs.writeFileSync(__dirname+"/"+settings.readme, readme.join("\n"));
